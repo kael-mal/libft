9f0541a2e56b0c6b94f97878750e4a0d6a7f36ab
kael-mal <maltikamil@gmail.com>
2025-10-16 12:10:15 +0200

    Add files via upload

---
+++ ft_strnstr.c
@@ -0,0 +1,24 @@
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t n)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if (!little[0])
+		return ((char *)big);
+	while (i < n && big[i])
+	{
+		j = 0;
+		while (big[i + j] && big[i + j] == little[j] && (i + j) < n)
+		{
+			if (!little[j + 1])
+				return ((char *)big + i);
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
